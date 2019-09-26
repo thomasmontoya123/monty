8530fe1a310e678fd17966683733a6b2d24214c2
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-26 19:40:38 +0000

    add.c

---
+++ add.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * _add - add two top elemnts
+ * @stack: stack head
+ * @line_number: line nomber
+ * return: void
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temporal;
+	int result = 0;
+
+	if (!(*stack))
+		fprintf(stderr, "L%d: can't add an empty stack", line_number),
+		exit(EXIT_FAILURE);
+
+	if (!(*stack)->next)
+		fprintf(stderr, "L%d: can't add, stack too short", line_number),
+		exit(EXIT_FAILURE);
+
+	temporal = *stack;
+
+	result = temporal->n + temporal->next->n;
+	temporal->next->n = result;
+	_pop(stack, line_number);
+}
