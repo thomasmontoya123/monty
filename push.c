d5c21303d6f0f11d7e25e70c90c87589f7da9880
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-26 19:51:34 +0000

    push.c

---
+++ push.c
@@ -0,0 +1,47 @@
+#include "monty.h"
+
+/**
+ * _push - adds a new node at the beginning
+ * @stack: stack head
+ * @line_number: line nomber
+ */
+
+void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+	int value = 0;
+	int i = 0;
+
+	new_node = _calloc(10, sizeof(stack_t));
+	if (!new_node)
+		fprintf(stderr, "Error: malloc failed"), exit(EXIT_FAILURE);
+
+	if (push_argument[0] == '-' || push_argument[0] == '+')
+		i = 1;
+
+	for (; push_argument[i] != '\0'; i++)
+	{
+		if (isdigit(push_argument[i]) == 0)
+			fprintf(stderr, "L%i: usage: push integer",
+			line_number), exit(EXIT_FAILURE);
+	}
+
+
+	value = atoi(push_argument);
+
+	new_node->n = value;
+	new_node->prev = NULL;
+
+	if (!*stack)
+	{
+		new_node->next = NULL;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+	}
+
+}
