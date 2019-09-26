e90dafa5eed3e160ef674992a3866fa3728e5e76
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-26 19:41:34 +0000

    free_handler.c

---
+++ free_handler.c
@@ -0,0 +1,17 @@
+#include"monty.h"
+
+/**
+ * free_handler - handles the free operations
+ * @input_line: full line
+ * @arguments: splited line
+ * @main_checker: return of function handler
+ * Return: void.
+ */
+
+void free_handler(char *input_line, char **arguments,
+instruction_t *main_checker)
+{
+	free(main_checker);
+	free(arguments);
+	free(input_line);
+}
