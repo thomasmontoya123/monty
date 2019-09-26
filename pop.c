4fa0538444a1f2c2c6ce9b3b69dbe49862943bc7
svelezg <47928168+svelezg@users.noreply.github.com>
2019-09-26 19:49:37 +0000

    pop.c

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _push - adds a new node at the beginning
+ * @stack: stack head
+ * @line_number: line nomber
+ */
+
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temporal;
+
+	if (!(*stack))
+		fprintf(stderr, "L%d: can't pop an empty stack", line_number),
+		exit(EXIT_FAILURE);
+
+	temporal = *stack;
+	*stack = temporal->next;
+		if (temporal->next)
+			temporal->next->prev = NULL;
+	free(temporal);
+
+}
