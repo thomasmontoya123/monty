d207206a942b94a84bf69ceb9047057041a8ec6b
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-26 14:46:35 -0500

    do nothing

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nope - do nothing
+ * @stack: stack head
+ * @line_number: line nomber
+ * return: void
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
