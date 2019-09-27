dda1c2f5464eca979cad1267b021c19ff087e16d
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-26 19:46:37 -0500

    without free

--- add.c
+++ add.c
@@ -14,11 +14,8 @@ void _add(stack_t **stack, unsigned int line_number)
 
 
 	if (!(*stack)->next || !*stack)
-	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number),
 		exit(EXIT_FAILURE);
-		free(stack);
-	}
 	temporal = *stack;
 
 	result = temporal->n + temporal->next->n;
